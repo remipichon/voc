249cf9456306c93ea26bcf420b662b1a85f387bb
RemiP <Remi.Pichon@pega.com>
2017-11-20 16:21:08 +0100

    test (runner app) add SI and SSI remote mode non trigger via context

--- core/app/test/test-suite-trigger-via-config-remote.js
+++ core/app/test/test-suite-trigger-via-config-remote.js
@@ -3,8 +3,6 @@ var TestCaseError = require("./TestCaseError");
 
 module.exports = {
 
-    //   x  |SI|SSI dry_run_do_all__remote_without_context__trigger_via_config
-
     image__dry_run_do_all__remote_without_context__trigger_via_config: function () {
 
         testUtil.prepare();
@@ -22,6 +20,63 @@ module.exports = {
                 "remote-repo: Successfully cloned remote repo ssh://root@127.0.0.1/root/git-server/remote-repo into [..] remote-repo __for remote-repo __once",
                 "docker build [..] Dockerfile.remote [..] remote-image __for remote-image __once"
             )) {
+        } else {
+            throw new TestCaseError(__test_case_name_1);
+        }
+    },
+
+    simple_stack_instance_docker_composes__dry_run_do_all__remote_without_context__trigger_via_config: function () {
+
+        testUtil.prepare();
+        testUtil.prepareRemoteRepo();
+
+        testUtil.copyGitAddFile(
+            "instances/simple-stack-instance.remote-repo.nominalcase.mynominalcase.json",
+            "remote-repo/repo.remote-repo.json"
+        );
+
+        testUtil.copyGitAddFileRemoteRepo("dockercomposes/docker-compose.nominalcase.yml");
+
+        testUtil.commit(" trigger via config and Docker related files ");
+
+        testUtil.run();
+
+        if (testUtil.assert(
+                "remote-repo: Successfully cloned remote repo ssh://root@127.0.0.1/root/git-server/remote-repo into [..] remote-repo __for remote-repo __once",
+                "Successfully config [..]  docker-compose.nominalcase.yml [..] docker-compose.intermediate.mynominalcase.yml __for mynominalcase __once",
+                "docker-compose [..] build [..] docker-compose.intermediate.mynominalcase.yml __for mynominalcase __once",
+                "docker stack deploy [..] docker-compose.intermediate.mynominalcase.yml mynominalcase __for mynominalcase __once",
+            )) {
+
+        } else {
+            throw new TestCaseError(__test_case_name_1);
+        }
+    },
+
+    stack_instance_stack_definition_docker_compose__dry_run_do_all__remote_without_context__trigger_via_config: function () {
+
+        testUtil.prepare();
+        testUtil.prepareRemoteRepo();
+
+        testUtil.copyGitAddFile(
+            "instances/stack-instance.remote-nominalcase.myremotenominalcase.json",
+            "stackdefinitions/stack-definition.remote-repo.remote-nominalcase.json",
+            "remote-repo/repo.remote-repo.json"
+        );
+
+        testUtil.copyGitAddFileRemoteRepo("dockercomposes/docker-compose.nominalcase.yml");
+
+        testUtil.commit(" trigger via config and Docker related files ");
+
+        testUtil.run();
+
+        if (testUtil.assert(
+                "remote-repo: Successfully cloned remote repo ssh://root@127.0.0.1/root/git-server/remote-repo into [..] remote-repo __for remote-repo __once",
+                "Successfully config [..] docker-compose.nominalcase.yml [..] docker-compose.intermediate.myremotenominalcase.yml __for myremotenominalcase __once",
+                "docker-compose [..] build [..] docker-compose.intermediate.myremotenominalcase.yml __for myremotenominalcase __once",
+                "docker stack deploy [..] docker-compose.intermediate.myremotenominalcase.yml myremotenominalcase __for myremotenominalcase __once",
+            )) {
+
         } else {
             throw new TestCaseError(__test_case_name_1);
         }
--- core/app/test/test-suite-trigger-via-context-remote.js
+++ core/app/test/test-suite-trigger-via-context-remote.js
@@ -3,8 +3,6 @@ var TestCaseError = require("./TestCaseError");
 
 module.exports = {
 
-    //   x  |SI|SSI dry_run_do_all__remote_with_context__non_trigger_via_context
-
     image__dry_run_do_all__remote_context__non_trigger_via_context: function () {
 
         testUtil.prepare();
@@ -15,7 +13,7 @@ module.exports = {
         testUtil.copyGitAddFileRemoteRepo("images/Dockerfile.remote-with_context");
         testUtil.copyGitAddFileRemoteRepo({source:"context/dummy_context_file", destination: "context"});
 
-        testUtil.commit(" trigger via context on remote repo");
+        testUtil.commit(" non trigger via context on remote repo");
 
         testUtil.run();
 
@@ -27,5 +25,57 @@ module.exports = {
         }
 
     },
+
+    simple_stack_instance_docker_composes__dry_run_do_all__remote_without_context__non_trigger_via_context: function () {
+
+        testUtil.prepare();
+        testUtil.prepareRemoteRepo();
+
+        testUtil.copyFile(
+            "instances/simple-stack-instance.remote-repo.withcontext.myremotewithcontext.json",
+            "remote-repo/repo.remote-repo.json"
+        );
+
+        testUtil.copyGitAddFileRemoteRepo("dockercomposes/docker-compose.withcontext.yml");
+        testUtil.copyGitAddFileRemoteRepo({source:"context/dummy_context_file", destination: "context"});
+
+        testUtil.commit(" non trigger via context on remote repo");
+
+        testUtil.run();
+
+        if (testUtil.assert(
+                "There was nothing to do so I did nothing __for VOC __once"
+            )) {
+        } else {
+            throw new TestCaseError(__test_case_name_1);
+        }
+    },
+
+    stack_instance_stack_definition_docker_compose__dry_run_do_all__remote_without_context__non_trigger_via_context: function () {
+
+        testUtil.prepare();
+        testUtil.prepareRemoteRepo();
+
+        testUtil.copyFile(
+            "instances/stack-instance.remote-withcontext.myremotewithcontext.json",
+            "stackdefinitions/stack-definition.remote-repo.remote-withcontext.json",
+            "remote-repo/repo.remote-repo.json"
+        );
+
+        testUtil.copyGitAddFileRemoteRepo("dockercomposes/docker-compose.nominalcase.yml");
+        testUtil.copyGitAddFileRemoteRepo({source:"context/dummy_context_file", destination: "context"});
+
+        testUtil.commit(" non trigger via context on remote repo");
+
+        testUtil.run();
+
+        if (testUtil.assert(
+                "There was nothing to do so I did nothing __for VOC __once"
+            )) {
+        } else {
+            throw new TestCaseError(__test_case_name_1);
+        }
+    },
+
 
 };
