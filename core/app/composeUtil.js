bf5a1cb896d07e293496a2444421a8cde569d794
wae_user <wae_user@mail.com>
2018-05-20 11:12:23 +0200

    debug (runner app) add compose debug

--- core/app/composeUtil.js
+++ core/app/composeUtil.js
@@ -3,12 +3,14 @@
 var _ = require("underscore");
 var utils = require("./utils");
 var dockerUtils = require("./dockerUtil");
+var log = require('loglevel');
 
 module.exports = {
     build: function (pathToCompose, dryRun = false) {
         const composeCmd = `${this.getComposeExec()} -f ${pathToCompose} build`;
         if(dryRun)
             return `Dry run: Docker would have run '${composeCmd}'`;
+        log.info("compose cmd",composeCmd);
         return utils.execCmdSync(composeCmd, true);
     },
 
